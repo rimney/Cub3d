6aed4ad51524b564909401f05c5fcdb6405015a7
rimney <rmneyz@gmail.com>
2022-12-27 21:18:52 +0100

    rimneys folder for practicing the rest of the project

---
+++ rimney/cube.h
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cube.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 01:30:45 by rimney            #+#    #+#             */
+/*   Updated: 2022/12/25 22:20:59 by rimney           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUBE_H
+# define CUBE_H
+# define SCALE 32
+
+#include <unistd.h>
+#include <mlx.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include "Get_next_line/get_next_line.h"
+
+typedef struct s_img
+{
+	void	*img;
+	int		endian;
+	int		bpp;
+	int		size_len;
+	char	*addr;
+} t_img;
+
+typedef	struct	s_cube
+{
+    double		P_position_x;
+    double		P_posotion_y;
+    char	*NO; // 0
+	char	*WE; // 2
+	char	*EA; // 3
+	char	*SO; // 1
+	int		C; // 4
+	int		F; // 5
+	int		*files_f;
+	int		Psp; // it contains the ascii value of the direction character
+	int		MapHeight;
+	int		MapWidth;
+	char	**Map;
+	void	*mlx_init;
+	void	*mlx_window;
+	t_img	*img;
+} t_cube;
+
+/********************** Parsing Functions **********************/
+
+void	ft_get_NO(t_cube *cube, char *line);
+void	ft_get_SO(t_cube *cube, char *line);
+void	ft_get_WE(t_cube *cube, char *line);
+void	ft_get_EA(t_cube *cube, char *line);
+void	ft_get_xpms(t_cube *cube, char **argv);
+int		*ft_files_f_init(void);
+char	*ft_strjoin_1(char *str, char c);
+int	ft_check_rgb_2(char *line);
+int	ft_get_rgb(char *line, int i);
+int	ft_count_elements(char *str, char c);
+int	ft_rgb_check(char *line);
+void	ft_get_C(t_cube *cube, char *line);
+void	ft_get_F(t_cube *cube, char *line);
+void	ft_get_CF(t_cube *cube, char **argv);
+int	ft_strcmp(char *s1, char *s2);
+int	ft_is_an_xpm(char *line);
+int	ft_get_width(char **str);
+int	ft_get_hight(char **str);
+void	ft_print_2d(char **str);
+void	ft_free_2d(char **str);
+void	ft_parse_map_2(t_cube *cube, char *map);
+void	ft_parse_map(t_cube *cube, char **argv, char *arg, int i);
+char	is_a_direction(char c);
+int	is_valid(char c);
+int	c_is_safe(t_cube *cube, size_t i, size_t j);
+int	ft_check_header_and_footer(t_cube *cube);
+int	ft_check_map(t_cube *cube);
+void	ft_get_player_position(t_cube *cube);
+void	ft_get_map(t_cube *cube, char **argv);
+void	ft_cube_values_init(t_cube *cube);
+t_cube	*ft_struct_init(char **argv, t_img *img);
+void	ft_free_parsing(t_cube *cube);
+void ft_print(char *str, char *s);
+void	ft_print_cube(t_cube *cube);
+void	ft_exit(char *str);
+
+#endif
---
+++ rimney/main.c
@@ -0,0 +1,254 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/17 01:30:23 by rimney            #+#    #+#             */
+/*   Updated: 2022/12/27 21:13:47 by rimney           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cube.h"
+#include "Get_next_line/get_next_line.h"
+
+void	render_background(t_cube *cube)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < cube->MapHeight * SCALE)
+	{
+		j = 0;
+		while(j < cube->MapWidth * SCALE)
+		{
+			mlx_pixel_put(cube->mlx_init, cube->mlx_window, j, i, 0xFCD088);
+			j++;
+		}
+		i++;
+	}
+}
+
+// void	my_pixel_put(t_cube *cube, int x, int y, int color)
+// {
+// 	char *pixel;
+// 	int i;
+
+// 	i = 0;
+// 	pixel = cube->img->addr + (y * cube->img->size_len + x * (cube->img->bpp / 8));
+// 	while (i >= 0)
+// 	{
+// 		if(cube->img->endian)
+// 			*pixel++ = (color >> i) & 0xFF;
+// 		else
+// 			*pixel++ = (color >> (cube->img->bpp - 8 - i)) & 0xFF;
+// 		i -= 8;
+// 	}
+// }
+
+void	my_pixel_put(t_cube *cube, int x, int y, int color)
+{
+	char    *pixel;
+
+    pixel = cube->img->addr + (y * cube->img->size_len + x * (cube->img->bpp / 8));
+	*(int *)pixel = color;
+}
+
+int render_block(t_cube *cube, int x, int y, int w, int h, int color)
+{
+	int i;
+	int j;
+
+	i = y;
+	while(i < y + h)
+	{
+		j = x;
+		while (j < w + x)
+		{
+			my_pixel_put(cube, j, i, color);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+void	ft_render_lines_vertical(t_cube *cube)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = SCALE;
+	while(j < cube->MapWidth * SCALE)
+	{
+		i = 0;
+		while(i < cube->MapHeight * SCALE)
+		{
+			my_pixel_put(cube, j, i, 0xFF0000);
+			i++;
+		}
+		j += SCALE;
+	}
+}
+
+void	ft_render_lines_horizental(t_cube *cube)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = SCALE;
+	while(j < cube->MapHeight * SCALE)
+	{
+		i = 0;
+		while(i < cube->MapWidth * SCALE)
+		{
+			my_pixel_put(cube, i, j, 0xFF0000);
+			i++;
+		}
+		j += SCALE;
+	}
+}
+
+void	ft_render_lines(t_cube *cube)
+{
+	ft_render_lines_vertical(cube);
+	ft_render_lines_horizental(cube);
+}
+
+void	ft_render_player(t_cube *cube)
+{
+	int i;
+	int j;
+
+	i = cube->P_position_x;
+	j = cube->P_posotion_y;
+	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE + 1, cube->P_posotion_y * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE + 2, cube->P_posotion_y * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y * SCALE + 1, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y * SCALE + 2, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE - 1, cube->P_posotion_y * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE - 2, cube->P_posotion_y * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y - 1 * SCALE, 0xFFFF00);
+	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y - 2 * SCALE, 0xFFFF00);
+}
+
+// void	ft_move_player_forward(t_cube *cube, int x, int y)
+// {
+// 	y = 0;
+// 	my_pixel_put(cube, cube->P_position_x * SCALE, cube->P_posotion_y * SCALE + y, 0xFFFF00);
+
+// }
+
+int	key_hook(int key, t_cube *cube)
+{
+	printf("%d <<\n", key);
+	if(key == 0)
+		printf("A\n");
+	if(key == 1)
+		printf("S\n");
+	if(key == 2)
+		printf("D\n");
+	if (key == 256)
+	{
+		// ft_move_player_forward(cube, cube->P_position_x * SCALE, cube->P_posotion_y * SCALE);
+		printf("W\n");
+	}
+	if (key == 123)
+		printf("LEFT\n");
+	if (key == 124)
+		printf("right\n");
+	if (key == 125)
+		printf("DOWN\n");
+	if (key == 126)
+		printf("UP\n");
+	if(cube)
+		return (0);
+	return (0);
+}
+
+int	render(t_cube *cube, t_img *img)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	img->addr = mlx_get_data_addr(cube->img->img, &img->bpp, &img->size_len, &img->endian);
+	while(i < cube->MapHeight)
+	{
+		j = 0;
+		while((size_t)j < ft_strlen(cube->Map[i]))
+		{
+			if(cube->Map[i][j] == '1')
+				render_block(cube, j * SCALE, i * SCALE, SCALE, SCALE, 0xFFFFFF);
+			if(cube->Map[i][j] == '0')
+				render_block(cube, j * SCALE, i * SCALE, SCALE, SCALE, 0x0000FF);
+			if(is_a_direction(cube->Map[i][j]))
+				render_block(cube, j * SCALE, i * SCALE, SCALE, SCALE, 0x0000FF);
+			j++;
+		}
+		i++;
+	}
+	ft_render_lines(cube);
+	ft_render_player(cube);
+	return (0);
+}
+
+
+void	ft_create_window(t_cube *cube, t_img *img)
+{
+	int i = 0;
+	int j = 0;
+
+	cube->mlx_init = mlx_init();
+	cube->mlx_window = mlx_new_window(cube->mlx_init, cube->MapWidth * SCALE, cube->MapHeight * SCALE, "cube");
+	img->img = mlx_new_image(cube->mlx_init, cube->MapWidth * SCALE, cube->MapHeight * SCALE);
+	render(cube, img);
+	mlx_key_hook(cube->mlx_window, key_hook, cube);
+	mlx_put_image_to_window(cube->mlx_init, cube->mlx_window, img->img ,i, j);
+	mlx_loop(cube->mlx_init);
+}
+
+void	ft_img_init(t_img *img)
+{
+	img->bpp = 0;
+	img->endian = 0;
+	img->size_len = 0;
+	img->addr = NULL;
+}
+
+t_cube	*ft_struct_init(char **argv, t_img *img)
+{
+	t_cube *cube;
+	cube = malloc(sizeof(t_cube));
+	ft_cube_values_init(cube);
+	cube->files_f = ft_files_f_init();
+	ft_get_xpms(cube, argv);
+	ft_get_CF(cube, argv);
+	ft_get_map(cube, argv);
+	ft_img_init(img);
+	cube->img = img;
+	ft_create_window(cube, img);
+
+	return (cube);
+}
+
+int main(int argc, char **argv)
+{
+    t_cube	*cube;
+	t_img	img;
+	if(argc !=  2)
+	{
+		printf("Check the args !\n");
+		exit(0);
+	}
+	cube = ft_struct_init(argv, &img);
+	ft_print_cube(cube);
+	ft_free_parsing(cube);
+    // system("leaks Cub3d");
+}
