c41e9c2fe907206f0434e6f94539dfd3c4817961
rmney <rmneyz@gmail.com>
2023-01-24 17:12:14 +0100

    trusted vestion

--- Cub_draw.c
+++ Cub_draw.c
@@ -6,7 +6,7 @@
 /*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 02:58:41 by mrobaii           #+#    #+#             */
-/*   Updated: 2023/01/24 03:58:43 by rimney           ###   ########.fr       */
+/*   Updated: 2023/01/24 17:11:17 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,12 +71,12 @@ double cast_horizntal(t_cube *cube, double angle)
 		ystep *= -1;
 	if (!is_right(angle) && xstep > 0)
 		xstep *= -1;
-	else if (is_right(angle) && xstep < 0)
+	else  if (is_right(angle) && xstep < 0)
 		xstep *= -1;
 		
 	xnext = xintercept;
 	ynext = yintercept;
-		if (!is_down(angle))
+		if(!is_down(angle))
 			ynext -= 1;
 	while (xnext >= 0 && xnext < cube->stable->width && ynext >= 0 && ynext < cube->stable->height)
 	{
@@ -92,7 +92,7 @@ double cast_horizntal(t_cube *cube, double angle)
 	return (INT_MAX);
 }
 
-double	cast_vertical(t_cube *cube, double angle)
+double cast_vertical(t_cube *cube, double angle)
 {
 
 	double xstep, ystep, xintercept, yintercept;
@@ -131,75 +131,44 @@ double	cast_vertical(t_cube *cube, double angle)
 
 int	get_color_from_img(t_img *img, int x, int y)
 {
-	char	*dst;
+	char	*ret;
 
-	dst = img->addr + (y * img->size_len + x
+	ret = img->addr + (y * img->size_len + x
 			* (img->bpp / 8));
-	return (*(unsigned int *)dst);
+	return (*(int *)ret);
 }
 
-double	ft_get_pdistance(t_cube *cube, t_ray *ray, int i)
+void	render3D(t_cube *cube, t_ray *ray, int i)
 {
-	return (ray[i].raydistance
-		* cos(ray[i].ray_angle - cube->player->angle));
-}
-
-double	ft_get_dpp(t_cube *cube)
-{
-	return ((WINDOW_WIDTH / 2) / tan(cube->stable->fov / 2));
-}
-
-double	ft_get_projectionwallheight(t_cube *cube, t_ray *ray, int i)
-{
-	return ((SCALE / ft_get_pdistance(cube, ray, i)) * ft_get_dpp(cube));
-}
-
-void	ft_draw(int j, int wsh, t_cube *cube, int i)
-{
-	int dt;
-
-	dt = j + (wsh / 2) - (WINDOW_HEIGHT / 2);
-	cube->texture_offs_y = dt * ((float)cube->texture->height / wsh);
-	cube->texture_color
-		= get_color_from_img(cube->texture,
-			cube->texture_offs_x, cube->texture_offs_y);
-	my_mlx_pixel_put(cube, i, j,
-		cube->texture_color);
-}
-
-int	get_wallbottompixel(int wallbottompixel)
-{
-	if(wallbottompixel > WINDOW_HEIGHT)
-		return (wallbottompixel);
-	return (wallbottompixel);
-}
-
-int	get_walltoppixel(int walltoppixel)
-{
-	if(walltoppixel < 0)
-		return (0);
-	return (walltoppixel);
-}
-
-void	render3D(t_cube *cube, t_ray *ray, int i, int j)
-{
-	int		wallstripeheight;
-	int		walltoppixel;
-	int		wallbottompixel;
-
-	wallstripeheight = (int)ft_get_projectionwallheight(cube, ray, i);
+	int j;
+	double pdistance;
+	double dpp;
+	double projectionwallheight;
+	int wallstripeheight;
+	int walltoppixel;
+	int wallbottompixel;
+	int distance_top;
+	
+	
+	pdistance = ray[i].raydistance * cos(ray[i].ray_angle - cube->player->angle);
+	dpp = (WINDOW_WIDTH / 2) / tan(cube->stable->fov / 2);
+	projectionwallheight = (SCALE / pdistance) * dpp;
+	wallstripeheight = (int)projectionwallheight;
 	walltoppixel = (WINDOW_HEIGHT / 2) - (wallstripeheight / 2);
-	walltoppixel = get_walltoppixel(walltoppixel);
+	walltoppixel =  walltoppixel < 0 ? 0 : walltoppixel;
 	wallbottompixel = (WINDOW_HEIGHT / 2) + (wallstripeheight / 2);
-	wallbottompixel = get_wallbottompixel(wallbottompixel);
+	wallbottompixel = wallbottompixel > WINDOW_HEIGHT ? WINDOW_HEIGHT: wallbottompixel;
 	j = walltoppixel;
-	if (ray[i].is_vertical_hit)
+	if(ray[i].is_vertical_hit)
 		cube->texture_offs_x = (int)ray[i].wall_hit_point_y % SCALE;
 	else
 		cube->texture_offs_x = (int)ray[i].wall_hit_point_x % SCALE;
-	while (j < wallbottompixel)
+	while(j < wallbottompixel)
 	{
-		ft_draw(j, wallstripeheight, cube, i);
+		distance_top = j + (wallstripeheight / 2) - (WINDOW_HEIGHT / 2);
+		cube->texture_offs_y = distance_top * ((float)cube->texture->height / wallstripeheight);
+		cube->texture_color = get_color_from_img(cube->texture, cube->texture_offs_x, cube->texture_offs_y);
+		my_mlx_pixel_put(cube, i, j, cube->texture_color);
 		j++;
 	}
 }
@@ -213,57 +182,55 @@ t_ray *cast_all_ray(t_cube *cube)
 	i = 0;
 	rays = malloc(sizeof(t_ray) * cube->stable->num_of_rays + 1);
 	angle = cube->player->angle - (cube->stable->fov / 2);
-	while (i < cube->stable->num_of_rays)
+	while (i < cube->stable->num_of_rays )
 	{
 		resize_angle(&angle);
-		horizdestance = cast_horizntal(cube, angle);
-		verdistance = cast_vertical(cube, angle);
+		horizdestance =  cast_horizntal(cube, angle);
+		verdistance =  cast_vertical(cube, angle);
 		if (horizdestance < verdistance)
 		{
 			rays[i].wall_hit_point_x = cube->player->horizx;
-			rays[i].wall_hit_point_y = cube->player->horizy;
+			rays[i].wall_hit_point_y =  cube->player->horizy;
 			rays[i].raydistance = horizdestance;
 			rays[i].ray_angle = angle;
 			rays[i].is_vertical_hit = 0;
-			if (rays[i].ray_angle >= 0 &&  rays[i].ray_angle <= M_PI)
+			if(rays[i].ray_angle >= 0 &&  rays[i].ray_angle <= M_PI)
 				cube->texture = cube->so_texture;
 			else
 				cube->texture = cube->no_texture;
 		}
 		else
 		{
-			rays[i].wall_hit_point_x = cube->player->verticalx;
+			rays[i].wall_hit_point_x =  cube->player->verticalx;
 			rays[i].wall_hit_point_y = cube->player->verticaly;
 			rays[i].raydistance = verdistance;
 			rays[i].is_vertical_hit = 1;
 			rays[i].ray_angle = angle;
-			if (rays[i].ray_angle >= M_PI / 2 && rays[i].ray_angle <= 3 * M_PI / 2)
+			if(rays[i].ray_angle >= M_PI / 2 && rays[i].ray_angle <= 3 * M_PI / 2)
 				cube->texture = cube->we_texture;
 			else
 				cube->texture = cube->ea_texture;
 		}
-		render3D(cube, rays, i, i);
+		render3D(cube, rays, i);
 		i++;
 		angle += cube->stable->fov / cube->stable->num_of_rays;
 	}
 	return (rays);
 }
 
-void	wall_projection(t_cube *cube)
-{	
+void floor_ceeling_render(t_cube *cube)
+{
 	rectengale(0, 0,WINDOW_WIDTH, WINDOW_HEIGHT / 2, cube, cube->c);
-	rectengale(0, WINDOW_HEIGHT / 2,
-		WINDOW_WIDTH, WINDOW_HEIGHT / 2, cube, cube->f);
+	rectengale(0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, WINDOW_HEIGHT / 2, cube, cube->f);
 }
 
-int	cub_draw(t_cube *cube)
+int cub_draw(t_cube *cube)
 {
 	ft_new_image(cube);
 	update_player(cube);
-	wall_projection(cube);
+	floor_ceeling_render(cube);
 	cast_all_ray(cube);
-	mlx_put_image_to_window(cube->mlx_init,
-		cube->mlx_window, cube->img->img, 0, 0);
+	mlx_put_image_to_window(cube->mlx_init, cube->mlx_window, cube->img->img, 0, 0);
 	mlx_destroy_image(cube->mlx_init, cube->img->img);
 	return (0);
 }
